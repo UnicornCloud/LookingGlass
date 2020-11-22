381310babc6c5bd2f89e9e7b4442da154451fc35
DarkStar <darkstar@unisocial.net>
2020-11-22 07:10:31 -0500

    works!

---
+++ .graph/readme.md
@@ -0,0 +1,3 @@
+
+# Solution
+https://forum.level1techs.com/t/looking-glass-triage/130952/1363
---
+++ Client/works.sh
@@ -0,0 +1,13 @@
+touch /dev/shm/looking-glass && chown libvirt-qemu:kvm /dev/shm/looking-glass && chmod 660 /dev/shm/looking-glass
+
+chmod 666 /dev/shm/looking-glass
+
+- name: apparmour
+lineinfile:
+path: /etc/apparmor.d/abstractions/libvirt-qemu
+line: '/dev/shm/looking-glass rw,'
+
+
+# Wayland for Lowest Latency
+Boot Ubuntu as Wayland! or Logout Login
+Solves: https://www.reddit.com/r/OpenMW/comments/i1xroo/failed_to_create_sdl_window_couldnt_find_matching/
